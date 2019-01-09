721ec44c4d431327030d52b9dadfa9bc059375cb
Robert Bolinger <rbolinger86@gmail.com>
2019-01-09 12:55:33 -0600

    Setting my create method for errors

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,4 +13,24 @@ class PinsController < ApplicationController
     render :show
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin.errors.add('input', 'must be only alphabetic characters') if @pin.to_s.chars.length > @pin.to_s.gsub(/[^a-z]/i, '').chars.length
+    if @pin.errors
+      render :show
+      @pin = Pin.create(pin_params)
+    else
+      render :new
+      @error = @pin.error
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :resource_type)
+  end
 end
