fe402fbea7dd8b91b1c8a977d0d476cfd441f8b9
Robert Bolinger <rbolinger86@gmail.com>
2019-01-17 18:30:32 -0600

    Getting Heroku working!

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  validates_presence_of :title, :url, :slug, :text, :category_id
+  validates_presence_of :title, :url, :slug, :text
   validates_uniqueness_of :slug
   belongs_to :category
   has_attached_file :image, styles: { medium: "300x300>", thumb: "60x60>" }, default_url: "http://placebear.com/300/300"
