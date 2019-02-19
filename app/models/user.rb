0c67f18cb6f861e90ff28e540c3dec86a9df0b00
Robert Bolinger <rbolinger86@gmail.com>
2019-02-19 13:01:18 -0600

    Making Authentication work!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   def self.authenticate(email, password)
-    if User.find(params[:email]) && User.find(params[:password])
-      redirect_to @user.show
+    if User.find_by_email(email) && User.find_by_password(password)
+      user = @user
     else
       user = nil
     end
