00e905a509538c49064c210389272c163e60f379
Robert Bolinger <rbolinger86@gmail.com>
2019-04-25 12:02:41 -0500

    Making Heroku Work!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,40 @@
-rails = Category.create name: "rails"
-ruby = Category.create name: "ruby"
 Pin.create(
   title: 'Rails Tutorial',
   url: 'https://www.railstutorial.org/',
   text: "The trusted standard in self-directed introductions to Ruby on Rails. A complete treatment of all the essentials
     required in most production Rails applications.",
   slug: "rails-tutorial",
-  category: rails)
+  category_id: 2)
 Pin.create(
   title: 'Rails for Zombies',
   url: 'http://railsforzombies.org',
   text: "A fun, gamified way to hone your Rails skills! Come on...who doesn't like fighting zombies?!",
   slug: "rails-for-zombies",
-  category: rails)
+  category_id: 2)
 Pin.create(
   title: 'Try Ruby',
   url: 'http://tryruby.org/',
   text: "An interactive, in-browser tutorial for the Ruby programming language. A thorough walkthrough of
     Ruby's nuances, geared toward beginners.",
   slug: "try-ruby",
-  category: ruby)
+  category_id: 1)
 Pin.create(
   title: 'Ruby Quiz',
   url: 'http://rubyquiz.org',
   text: "A collection of quizzes on the Ruby programming language.",
   slug: "ruby-quiz",
-  category: ruby)
+  category_id: 1)
 Pin.create(
   title: 'Ruby on Rails for Developers',
   url: 'https://github.com/generalassembly/ga-ruby-on-rails-for-devs',
   text: "Somewhat advanced curriculum, but the exercises are also good for anyone who is motivated and
     self-guided with experience.",
   slug: "ga-ror-for-developers",
-  category: rails)
+  category_id: 2)
 Pin.create(
   title: 'Ruby Monk',
   url: 'http://rubymonk.com',
   text: "The Ruby Primer is a free interactive book that helps you learn Ruby. Discover Ruby idioms, learn
     lessons and solve problems, all in your browser!",
   slug: "ruby-monk",
-  category: ruby)
+  category_id: 1)
